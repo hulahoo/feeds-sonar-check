cf2a4447d5940a502f511dba60bed3d7b7a15402
Yuriy Starovoyt <yuriy.starovoyt@ru.axept.com>
2022-06-28 20:32:19 +0300

    Cleaned up models | Register m2m fields in admin to correct displaying refs #1

--- threatintel/intelhandler/admin.py
+++ threatintel/intelhandler/admin.py
@@ -2,19 +2,29 @@ from django.contrib import admin
 from .models import (
     Feed,
     Indicator,
-    MispEvent,
-    MispObject,
-    Attribute,
     Tag,
-    OrganizationContact,
 )
 
-# Register your models here.
 
-admin.site.register(Indicator)
-admin.site.register(Feed)
-admin.site.register(MispEvent)
-admin.site.register(MispObject)
-admin.site.register(Attribute)
+class IndicatorInline(admin.TabularInline):
+    model = Indicator
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+
+class FeedAdmin(admin.ModelAdmin):
+    inlines = (
+        IndicatorInline,
+        TagInline,
+    )
+
+
+class IndicatorAdmin(admin.ModelAdmin):
+    inlines = (TagInline,)
+
+
+admin.site.register(Indicator, IndicatorAdmin)
+admin.site.register(Feed, FeedAdmin)
 admin.site.register(Tag)
-admin.site.register(OrganizationContact)
--- threatintel/intelhandler/models.py
+++ threatintel/intelhandler/models.py
@@ -72,6 +72,10 @@ class BaseModel(models.Model):
 
 
 class Tag(BaseModel):
+    """
+    Модель тега.
+    """
+
     name = models.CharField("Название тега", max_length=30)
     colour = models.CharField("Название тега", max_length=30, blank=True, null=True)
     exportable = models.BooleanField(blank=True, null=True)
@@ -84,212 +88,11 @@ class Tag(BaseModel):
         verbose_name_plural = "Теги"
 
 
-class OrganizationContact(BaseModel):
-    name = models.CharField("Название организации", max_length=64)
-    uuid = models.CharField("Уникальный идентификатор", max_length=36, primary_key=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    class Meta:
-        verbose_name = "Контакт организации"
-        verbose_name_plural = "Контакты организаций"
-
-
-class Attribute(BaseModel):
-    type = models.CharField("Тип", max_length=30)
-    value = models.CharField("Значение", max_length=128)
-    timestamp = models.CharField("Временная отметка", max_length=10)
-    to_ids = models.BooleanField(blank=True, null=True)
-    disable_correlation = models.BooleanField(blank=True, null=True)
-    object_relation = models.CharField("Отношения объектов", max_length=15)
-    category = models.CharField("Категория аттрибута", max_length=30)
-    comment = models.CharField("Комментарий", max_length=128, blank=True, null=True)
-    uuid = models.CharField("Уникальный идентификатор", max_length=36, primary_key=True)
-    template_uuid = models.CharField(
-        "Уникальный идентификатор темплейта", max_length=36, blank=True, null=True
-    )
-    object_relation = models.CharField("Отношение к объекту", max_length=30)
-    value = models.CharField("Значение", max_length=128)
-
-    def __str__(self):
-        return f"{self.uuid}"
-
-    class Meta:
-        verbose_name = "Атрибут"
-        verbose_name_plural = "Атрибуты"
-
-
-class MispObject(BaseModel):
-    type = models.CharField("Тип аттрибута", max_length=30)
-    timestamp = models.CharField("Временная отметка", max_length=10)
-    template_version = models.CharField("Версия темплейта", max_length=10)
-    sharing_group_id = models.CharField("ID группы распространения", max_length=10)
-    description = models.CharField("Описание", max_length=128)
-    deleted = models.BooleanField(blank=True, null=True)
-    name = models.CharField("Название", max_length=32, unique=True)
-    category = models.CharField("Категория аттрибута", max_length=30)
-    comment = models.CharField("Комментарий", max_length=128, blank=True, null=True)
-    uuid = models.CharField("Уникальный идентификатор", max_length=36, primary_key=True)
-    object_relation = models.CharField("Отношение к объекту", max_length=30)
-    value = models.CharField("Значение", max_length=128)
-    attribute = models.ManyToManyField(
-        Attribute,
-        verbose_name="Аттрибут MISP Object'а",
-        related_name="misp_object_attribute",
-    )
-
-    def __str__(self):
-        return f"{self.uuid}"
-
-    class Meta:
-        verbose_name = "MISP объект"
-        verbose_name_plural = "MISP объекты"
-
-
-class MispEvent(BaseModel):
-    threat_level_id = models.CharField("Id уровня угрозы", max_length=1)
-    timestamp = models.CharField("Временная отметка последнего действия", max_length=10)
-    info = models.CharField("Временная отметка", max_length=256)
-    publish_timestamp = models.CharField("Временная отметка публикации", max_length=10)
-    date = models.DateField("Дата возникновения")
-    published = models.BooleanField("Опубликовано")
-    analysis = models.CharField("Анализ", max_length=1)
-    uuid = models.CharField("Уникальный идентификатор", max_length=36, primary_key=True)
-    orgc = models.ForeignKey(OrganizationContact, on_delete=models.SET_NULL, null=True)
-    tag = models.ManyToManyField(
-        Tag, verbose_name="Теги", related_name="misp_event_tags"
-    )
-    attribute = models.ManyToManyField(
-        Attribute,
-        verbose_name="Аттрибут MISP Event'а",
-        related_name="misp_event_attribute",
-    )
-    object = models.ManyToManyField(
-        MispObject, verbose_name="Объект MISP Event'а", related_name="misp_event_object"
-    )
-
-    def __str__(self):
-        return f"{self.info} | {self.uuid}"
-
-    class Meta:
-        verbose_name = "MISP событие"
-        verbose_name_plural = "MISP события"
-
-
-class Feed(BaseModel):
-    MISP = "MISP"
-    EMAIL_FROM = "FEMA"
-    EMAIL_SUBJECT = "SEMA"
-    MD5_HASH = "MD5H"
-    SHA1_HASH = "SHA1"
-    SHA256_HASH = "SHA2"
-    IP = "IPAD"
-    URL = "URLS"
-    DOMAIN = "DOMN"
-    FILENAME = "FILE"
-    REGISTRY = "REGS"
-
-    CSV_FILE = "CSV"
-    JSON_FILE = "JSN"
-    XML_FILE = "XML"
-    TXT_FILE = "TXT"
-
-    NO_AUTH = "NAU"
-    API = "API"
-    BASIC = "BSC"
-    NEVER = "NVR"
-    THIRTY_MINUTES = "M30"
-    ONE_HOUR = "HR1"
-    TWO_HOURS = "HR2"
-    FOUR_HOURS = "HR4"
-    EIGHT_HOURS = "HR8"
-    SIXTEEN_HOURS = "H16"
-    TWENTY_FOUR_HOURS = "H24"
-
-    TYPE_OF_FEED_CHOICES = [
-        (EMAIL_FROM, "Email's origin"),
-        (EMAIL_SUBJECT, "Email's subject"),
-        (MD5_HASH, "File hashe MD5"),
-        (SHA1_HASH, "File hashe SHA1"),
-        (SHA256_HASH, "File hashe SHA256"),
-        (FILENAME, "File name"),
-        (REGISTRY, "Registry"),
-        (IP, "IP adresses"),
-        (URL, "Full URL's"),
-        (DOMAIN, "Domain's"),
-    ]
-    FORMAT_OF_FEED_CHOICES = [
-        (CSV_FILE, "CSV формат"),
-        (JSON_FILE, "JSON формат"),
-        (XML_FILE, "XML формат"),
-        (TXT_FILE, "TXT формат"),
-    ]
-    TYPE_OF_AUTH_CHOICES = [
-        (NO_AUTH, "Отсуствует"),
-        (API, "API token"),
-        (BASIC, "HTTP basic"),
-    ]
-    POLLING_FREQUENCY_CHOICES = [
-        (NEVER, "Никогда"),
-        (THIRTY_MINUTES, "30 минут"),
-        (ONE_HOUR, "1 час"),
-        (TWO_HOURS, "2 часа"),
-        (FOUR_HOURS, "4 часа"),
-        (EIGHT_HOURS, "8 часов"),
-        (SIXTEEN_HOURS, "16 часов"),
-        (TWENTY_FOUR_HOURS, "24 часа"),
-    ]
-
-    type_of_feed = models.CharField(
-        "Тип фида", max_length=4, choices=TYPE_OF_FEED_CHOICES, default=IP
-    )
-    format_of_feed = models.CharField(
-        "Формат фида", max_length=3, choices=FORMAT_OF_FEED_CHOICES, default=TXT_FILE
-    )
-    auth_type = models.CharField(
-        "Тип авторизации", max_length=3, choices=TYPE_OF_AUTH_CHOICES, default=NO_AUTH
-    )
-    polling_frequency = models.CharField(
-        "Частота обновления фида",
-        max_length=3,
-        choices=POLLING_FREQUENCY_CHOICES,
-        default=NEVER,
-    )
-
-    auth_login = models.CharField(
-        "Логин для авторизации", max_length=32, blank=True, null=True
-    )
-    auth_password = models.CharField(
-        "Пароль для авторизации", max_length=64, blank=True, null=True
-    )
-    ayth_querystring = models.CharField(
-        "Строка для авторизации", max_length=128, blank=True, null=True
-    )
-    separator = models.CharField(
-        "Разделитель для CSV формата", max_length=8, blank=True, null=True
-    )
-    field_names = models.CharField(
-        "Названия полей через запятую", max_length=128, blank=True, null=True
-    )
-    sertificate = models.FileField("Файл сертификат", blank=True, null=True)
-    vendor = models.CharField("Вендор", max_length=32)
-    name = models.CharField("Название фида", max_length=32, unique=True)
-    link = models.CharField("Ссылка на фид", max_length=100)
-    confidence = models.IntegerField(
-        "Достоверность", validators=[MaxValueValidator(100), MinValueValidator(0)]
-    )
-    records_quantity = models.IntegerField("Количество записей", blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
-    class Meta:
-        verbose_name = "Фид"
-        verbose_name_plural = "Фиды"
-
-
 class Indicator(BaseModel):
+    """
+    Модель индикатора.
+    """
+
     EMAIL_FROM = "FEMA"
     EMAIL_SUBJECT = "SEMA"
     MD5_HASH = "MD5H"
@@ -461,3 +264,122 @@ class Indicator(BaseModel):
     class Meta:
         verbose_name = "Индикатор"
         verbose_name_plural = "Индикаторы"
+
+
+class Feed(BaseModel):
+    """
+    Модель фида - источника данных.
+    """
+
+    MISP = "MISP"
+    EMAIL_FROM = "FEMA"
+    EMAIL_SUBJECT = "SEMA"
+    MD5_HASH = "MD5H"
+    SHA1_HASH = "SHA1"
+    SHA256_HASH = "SHA2"
+    IP = "IPAD"
+    URL = "URLS"
+    DOMAIN = "DOMN"
+    FILENAME = "FILE"
+    REGISTRY = "REGS"
+
+    CSV_FILE = "CSV"
+    JSON_FILE = "JSN"
+    XML_FILE = "XML"
+    TXT_FILE = "TXT"
+
+    NO_AUTH = "NAU"
+    API = "API"
+    BASIC = "BSC"
+    NEVER = "NVR"
+    THIRTY_MINUTES = "M30"
+    ONE_HOUR = "HR1"
+    TWO_HOURS = "HR2"
+    FOUR_HOURS = "HR4"
+    EIGHT_HOURS = "HR8"
+    SIXTEEN_HOURS = "H16"
+    TWENTY_FOUR_HOURS = "H24"
+
+    TYPE_OF_FEED_CHOICES = [
+        (EMAIL_FROM, "Email's origin"),
+        (EMAIL_SUBJECT, "Email's subject"),
+        (MD5_HASH, "File hashe MD5"),
+        (SHA1_HASH, "File hashe SHA1"),
+        (SHA256_HASH, "File hashe SHA256"),
+        (FILENAME, "File name"),
+        (REGISTRY, "Registry"),
+        (IP, "IP adresses"),
+        (URL, "Full URL's"),
+        (DOMAIN, "Domain's"),
+    ]
+    FORMAT_OF_FEED_CHOICES = [
+        (CSV_FILE, "CSV формат"),
+        (JSON_FILE, "JSON формат"),
+        (XML_FILE, "XML формат"),
+        (TXT_FILE, "TXT формат"),
+    ]
+    TYPE_OF_AUTH_CHOICES = [
+        (NO_AUTH, "Отсуствует"),
+        (API, "API token"),
+        (BASIC, "HTTP basic"),
+    ]
+    POLLING_FREQUENCY_CHOICES = [
+        (NEVER, "Никогда"),
+        (THIRTY_MINUTES, "30 минут"),
+        (ONE_HOUR, "1 час"),
+        (TWO_HOURS, "2 часа"),
+        (FOUR_HOURS, "4 часа"),
+        (EIGHT_HOURS, "8 часов"),
+        (SIXTEEN_HOURS, "16 часов"),
+        (TWENTY_FOUR_HOURS, "24 часа"),
+    ]
+
+    type_of_feed = models.CharField(
+        "Тип фида", max_length=4, choices=TYPE_OF_FEED_CHOICES, default=IP
+    )
+    format_of_feed = models.CharField(
+        "Формат фида", max_length=3, choices=FORMAT_OF_FEED_CHOICES, default=TXT_FILE
+    )
+    auth_type = models.CharField(
+        "Тип авторизации", max_length=3, choices=TYPE_OF_AUTH_CHOICES, default=NO_AUTH
+    )
+    polling_frequency = models.CharField(
+        "Частота обновления фида",
+        max_length=3,
+        choices=POLLING_FREQUENCY_CHOICES,
+        default=NEVER,
+    )
+
+    auth_login = models.CharField(
+        "Логин для авторизации", max_length=32, blank=True, null=True
+    )
+    auth_password = models.CharField(
+        "Пароль для авторизации", max_length=64, blank=True, null=True
+    )
+    ayth_querystring = models.CharField(
+        "Строка для авторизации", max_length=128, blank=True, null=True
+    )
+    separator = models.CharField(
+        "Разделитель для CSV формата", max_length=8, blank=True, null=True
+    )
+    custom_field = models.CharField(
+        "Кастомное поле", max_length=128, blank=True, null=True
+    )
+    sertificate = models.FileField("Файл сертификат", blank=True, null=True)
+    vendor = models.CharField("Вендор", max_length=32)
+    name = models.CharField("Название фида", max_length=32, unique=True)
+    link = models.CharField("Ссылка на фид", max_length=100)
+    confidence = models.IntegerField(
+        "Достоверность", validators=[MaxValueValidator(100), MinValueValidator(0)]
+    )
+    records_quantity = models.IntegerField("Количество записей", blank=True, null=True)
+    indicators = models.ManyToManyField(
+        Indicator, related_name="feeds", verbose_name="Индикатор"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Фид"
+        verbose_name_plural = "Фиды"
