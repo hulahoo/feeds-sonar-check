fabbce76a1b9c7e36656e3a1dbda1f3826fc2126
Yuriy Starovoyt <yuriy.starovoyt@ru.axept.com>
2022-06-29 17:59:47 +0300

    Removed wrong registration of m2m fields. refs #1

--- threatintel/intelhandler/admin.py
+++ threatintel/intelhandler/admin.py
@@ -5,26 +5,6 @@ from .models import (
     Tag,
 )
 
-
-class IndicatorInline(admin.TabularInline):
-    model = Indicator
-
-
-class TagInline(admin.TabularInline):
-    model = Tag
-
-
-class FeedAdmin(admin.ModelAdmin):
-    inlines = (
-        IndicatorInline,
-        TagInline,
-    )
-
-
-class IndicatorAdmin(admin.ModelAdmin):
-    inlines = (TagInline,)
-
-
-admin.site.register(Indicator, IndicatorAdmin)
-admin.site.register(Feed, FeedAdmin)
+admin.site.register(Indicator)
+admin.site.register(Feed)
 admin.site.register(Tag)
