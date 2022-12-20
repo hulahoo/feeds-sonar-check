832e51b3d89db5080e2df6d3dd6672074e291185
Roman Lyashschuk <roman.lyaschuk@ru.axept.com>
2022-12-20 17:01:58 +0700

    worker start fix

--- src/feeds_importing_worker/main.py
+++ src/feeds_importing_worker/main.py
@@ -1,6 +1,7 @@
 """Main file for running the service"""
 
 import subprocess
+import os
 from threading import Thread
 
 from feeds_importing_worker.config.log_conf import logger
@@ -9,22 +10,26 @@ from feeds_importing_worker.web.routers.api import execute as flask_app
 
 
 def start_worker():
-    subprocess.run(["dagit", "-f", "feeds_importing_worker/worker.py"], check=True)
-
+    path = os.path.dirname(os.path.abspath(__file__))
+    subprocess.run(['dagit', '-f', f'{path}/worker.py'], check=True)
 
 
 def execute() -> None:
-    """Run the service.
+    """
+    Run the service.
 
     1. Create migrations;
     2. Run Flask thread;
     3. Run Worker thread.
     """
+    logger.info("Run migrations...")
     create_migrations()
-    logger.info("Start Flask...")
-    # remove later
-    # worker_thread: Thread = Thread(target=start_worker)
+
+    worker_thread: Thread = Thread(target=start_worker)
     flask_thread: Thread = Thread(target=flask_app)
 
+    logger.info("Start Flask...")
     flask_thread.start()
-    # worker_thread.start()
+
+    logger.info("Start Dagit...")
+    worker_thread.start()
